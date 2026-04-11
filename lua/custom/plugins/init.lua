ca54c6e75099b42e491b7aea755cfc36c95cc0cd
Chance Rebholz <crebh@amazon.com>
2026-04-11 18:36:12 +0000

    feat: md render

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -73,4 +73,15 @@ return {
   {
     'tpope/vim-fugitive',
   },
-}
+  {
+    "mfussenegger/nvim-jdtls"
+  },
+{
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}}
